9ede882d1737cd7a0a93c051a6cbada2887050c0
Markus Englund <markus.s.englund@gmail.com>
2016-12-08 22:26:54 +0100

    ip fix

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ app.get("/", function(req, res) {
     //console.log("ip: " + req.ip)
     var ua = parser(req.headers["user-agent"]);
     var language = req.headers["accept-language"].split(",")[0];
-    res.json({ipaddress: req.ip, language: language, os:ua.os.name + " " + ua.os.version});
+    var ip = req.headers["x-forwarded-for"];
+    res.json({
+        reqip: req.ip, 
+        xforward: ip, 
+        reqconnection: req.connection.remoteAddress,
+        language: language, 
+        os:ua.os.name + " " + ua.os.version});
 });
 
 app.listen(port, function() {
