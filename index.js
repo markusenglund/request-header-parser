7852969d6d329d5d28913226f03e2f7db0ea5955
Markus Englund <markus.s.englund@gmail.com>
2016-12-08 15:52:38 +0100

    small changes

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var express = require("express"),
-    app = express();
+    app = express(),
+    parser = require("ua-parser-js");
 
 
 app.get("/", function(req, res) {
-    res.json({message: "Hello World!"});
-    console.log("Hello World!");
+    
+    console.log("ip: " + req.ip)
+    var ua = parser(req.headers["user-agent"]);
+    console.log("os: " + ua.os);
+    var language = req.headers["accept-language"].split(",")[0];
+    res.json({ipaddress: req.ip, language: language, os:ua.os.name + " " + ua.os.version});
 })
 
 app.listen(3000, function() {
