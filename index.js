e5dc6329432f00c652465d5caa986e9da4a4f384
Markus Englund <markus.s.englund@gmail.com>
2016-12-08 16:37:53 +0100

    changed port

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var express = require("express"),
     app = express(),
-    parser = require("ua-parser-js");
+    parser = require("ua-parser-js"),
+    port = process.env.PORT || 3000
 
 
 app.get("/", function(req, res) {
     
-    console.log("ip: " + req.ip)
+    //console.log("ip: " + req.ip)
     var ua = parser(req.headers["user-agent"]);
-    console.log("os: " + ua.os);
     var language = req.headers["accept-language"].split(",")[0];
     res.json({ipaddress: req.ip, language: language, os:ua.os.name + " " + ua.os.version});
 })
 
-app.listen(3000, function() {
+app.listen(port, function() {
     console.log("Now listening");
 });
